c2e35726e2d0c1dc70fa83162a6651f6de65ff64
Paige Winnett <winnettpaige2@gmail.com>
2024-09-14 11:11:56 -0500

    first commit

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,37 @@
+using Xunit;
+using Prime.Services;
+
+namespace Prime.UnitTests.Services
+{
+    public class PrimeService_IsPrimeShould
+    {
+        private readonly PrimeService _primeService;
+
+public PrimeService_IsPrimeShould()
+{
+    _primeService = new PrimeService();
+}
+       [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    [InlineData(1)]
+public void IsPrime_ValuesLessThan2_ReturnFalse(int value)
+{
+    var result = _primeService.IsPrime(value);
+
+    Assert.False(result, $"{value} should not be prime");
+}
+    }
+}
+public bool IsPrime(int candidate)
+{
+    if (candidate < 2)
+    {
+        return false;
+    }
+    throw new NotImplementedException("Not fully implemented.");
+}
+var primeService = new PrimeService();
+bool result = primeService.IsPrime(1);
+
+Assert.False(result, "1 should not be prime");
